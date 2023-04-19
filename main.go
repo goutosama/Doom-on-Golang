d75c8b9799949d0992be430f5256e2e6ed5c97eb
Fagirton <fagirton2015@gmail.com>
2023-04-19 23:56:07 +0300

    IT WORKS??????

--- main.go
+++ main.go
@@ -7,18 +7,21 @@ import (
 	wad "github.com/fagirton/Doom-on-Golang/wad_reader"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+var (
+	num1 string
+	num2 int32
+	num3 int32
 )
 
 func init() {
-	reader := wad.NewReader("/doom1.wad")
-	var (
-		num1 string
-		num2 string
-		num3 string
-	)
-	num1 = reader.ReadString(0)
-	num2 = string(reader.Read4Bytes(4))
-	num3 = string(reader.Read4Bytes(8))
+	reader := wad.NewReader("doom1.wad")
+
+	num1 = reader.ReadString(0, 4)
+	num2 = reader.ReadInt32(4)
+	num3 = reader.ReadInt32(8)
 	fmt.Println(num1, num2, num3)
 }
 
@@ -29,6 +32,7 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, num1)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
--- wad_reader/wad_reader.go
+++ wad_reader/wad_reader.go
@@ -1,6 +1,8 @@
 package wad
 
 import (
+	"encoding/binary"
+	"io"
 	"os"
 )
 
@@ -17,20 +19,92 @@ func (s WadReader) CloseReader(w WadReader) error {
 	return w.wad_file.Close()
 }
 
-func (s WadReader) Read4Bytes(offset int64) []byte {
-	_, err0 := s.wad_file.Seek(offset, 0)
-	if err0 == nil {
-		panic(err0)
-	}
-	var buffer []byte = []byte{0, 0, 0, 0}
-	_, err1 := s.wad_file.Read(buffer)
-	if err1 == nil {
-		panic(err1)
-	}
+func (s WadReader) ReadBytes(offset int64, num_bytes int) []byte {
+	s.wad_file.Seek(offset, 0)
+	buffer := make([]byte, num_bytes)
+	io.ReadAtLeast(s.wad_file, buffer, num_bytes)
 	return buffer
 }
 
-func (s WadReader) ReadString(offset int64) string {
-	byt := s.Read4Bytes(offset)
+func (s WadReader) ReadString(offset int64, num_bytes int) string {
+	byt := s.ReadBytes(offset, num_bytes)
 	return string(byt)
+}
+
+func (s WadReader) ReadInt32(offset int64) int32 {
+	byt := s.ReadBytes(offset, 4)
+	return int32(binary.LittleEndian.Uint32(byt))
+}
+
+func (s WadReader) ReadInt16(offset int64) int16 {
+	byt := s.ReadBytes(offset, 2)
+	return int16(binary.LittleEndian.Uint16(byt))
+}
+
+type Header struct {
+	signature    string
+	lumps_number int32
+	table_offset int32
+}
+
+func (s WadReader) ReadHeader() Header {
+	return Header{
+		signature:    s.ReadString(0, 4),
+		lumps_number: s.ReadInt32(4),
+		table_offset: s.ReadInt32(8),
+	}
+}
+
+type Lump struct {
+	offset int32
+	size   int32
+	name   string
+}
+
+func (s WadReader) Find_lump_index_by_name(directory []Lump, n string) int {
+	for i := 0; i < len(directory); i++ {
+		if directory[i].name == n {
+			return i
+		}
+	}
+	return -1
+}
+
+func (s WadReader) ReadDirectory(h Header) []Lump {
+	var directory []Lump
+	for i := 0; i < int(h.lumps_number); i++ {
+		curr_offset := int64(h.table_offset) + int64(i*16)
+		l := Lump{
+			offset: s.ReadInt32(curr_offset),
+			size:   s.ReadInt32(curr_offset + 4),
+			name:   s.ReadString(curr_offset+8, 8),
+		}
+		directory = append(directory, l)
+	}
+	return directory
+}
+
+type Vertex struct {
+	x int16
+	y int16
+}
+
+func (s WadReader) ReadVertex(offset int64) Vertex {
+	return Vertex{
+		x: s.ReadInt16(offset),
+		y: s.ReadInt16(offset + 2),
+	}
+}
+
+var Lump_class = map[string]int{ //можно поменять на срез и его индекс+1
+	"THINGS":   1,
+	"LINEDEFS": 2,
+	"SIDEDEFS": 3,
+	"VERTEXES": 4,
+	"SEGS":     5,
+	"SSECTORS": 6,
+	"NODES":    7,
+	"SECTORS":  8,
+	"REJECT":   9,
+	"BLOCKMAP": 10,
 }
